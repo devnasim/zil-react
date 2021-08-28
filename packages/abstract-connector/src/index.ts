59e6d2c9572005c272577b576195ed6081535191
Nasim Akhtar <devnasim18@gmail.com>
2021-08-28 21:26:32 +0530

    added Abstract Connector

---
+++ packages/abstract-connector/src/index.ts
@@ -0,0 +1,45 @@
+import { EventEmitter } from 'events';
+import { AbstractConnectorArguments, ConnectorUpdate, ConnectorEvent } from '@web3-react/types';
+
+// eslint-disable-next-line no-underscore-dangle
+declare let __DEV__: boolean;
+// eslint-disable-next-line import/prefer-default-export
+export abstract class AbstractConnector extends EventEmitter {
+  public readonly supportedNetworksIds?: number[];
+
+  constructor({ supportedChainIds }: AbstractConnectorArguments = {}) {
+    super();
+    this.supportedNetworksIds = supportedChainIds;
+  }
+
+  public abstract activate(): Promise<ConnectorUpdate>;
+
+  public abstract getProvider(): Promise<any>;
+
+  public abstract getChainId(): Promise<number | string>;
+
+  public abstract getAccount(): Promise<null | string>;
+
+  public abstract deactivate(): void;
+
+  protected emitUpdate(update: ConnectorUpdate): void {
+    if (__DEV__) {
+      console.log(`Emitting '${ConnectorEvent.Update}' with payload`, update);
+    }
+    this.emit(ConnectorEvent.Update, update);
+  }
+
+  protected emitError(error: Error): void {
+    if (__DEV__) {
+      console.log(`Emitting '${ConnectorEvent.Error}' with payload`, error);
+    }
+    this.emit(ConnectorEvent.Error, error);
+  }
+
+  protected emitDeactivate(): void {
+    if (__DEV__) {
+      console.log(`Emitting '${ConnectorEvent.Deactivate}'`);
+    }
+    this.emit(ConnectorEvent.Deactivate);
+  }
+}
